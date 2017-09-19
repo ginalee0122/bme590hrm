470150564b86a77050f4721462058004d0385359
Leo Hu <ch289@duke.edu>
2017-09-19 16:15:57 -0400

    update unit test file.

--- test_instHR.py
+++ test_instHR.py
@@ -1,5 +1,5 @@
 import sys, os, string
-import instHR
+from instHR import *
 
 
 def test_getPeaks():
@@ -7,10 +7,10 @@ def test_getPeaks():
     mV = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
     time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     threshV = 2
-    assert getPeaks(mV,time,threshV) == [3, 3, 3, 3, 7, 11]
+    assert getPeaks(mV, time, threshV) == [3, 3, 3, 3, 7, 11]
     mV2 = [0, 1, 2, 4, 0, 1, 2.5, 3.1, 0, 1, 2, 3]
     time2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    assert getPeaks(mV2,time2,threshV) == [4, 2.5, 3, 3, 6, 11]
+    assert getPeaks(mV2, time2, threshV) == [4, 2.5, 3, 3, 6, 11]
     mV3 = [0, 1, 2, 4, 0, 1, 2.5, 3.1, 0, 1, 2, 3]
     time3 = [0, 2, 4, 6, 8, 9, 10, 11, 12, 13]
     assert getPeaks(mV2, time2, threshV) == [4, 2.5, 3, 6, 10, 13]
