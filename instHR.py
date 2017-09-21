cd98f73e7f35d1f7243fafddbdbad32a6641c3c0
Leo Hu <ch289@duke.edu>
2017-09-21 15:12:34 -0400

    test_instHR updated for travis

--- instHR.py
+++ instHR.py
@@ -5,10 +5,10 @@ import sys, os, string
 def getPeaks(vs, ts, threshV=1):
     """This function loops through lists of Voltage and Time and returns two arrays,
     one containing the voltage peaks, and one with their respective time points."""
-    PeakVs = []
+    # PeakVs = []
     PeakTs = []
-    inV = 0
-    inT = 0
+    # inV = 0
+    # inT = 0
     threshPassedBool = False
 
     for i in range(0,len(vs)):
@@ -16,19 +16,27 @@ def getPeaks(vs, ts, threshV=1):
         inT = ts[i]
         if inV > threshV and not threshPassedBool:
             threshPassedBool = True
-            PeakVs.append(inV)
+            # PeakVs.append(inV)
             PeakTs.append(inT)
         if inV < threshV:
             threshPassedBool = False
 
-    return PeakVs, PeakTs
+    return PeakTs
+
+
+def instHR(pTs, instT=1):
+    if instT < 1:
+        instT = 1
+    # compute instantaneous BPM using previous time and current time
+    timeDiff = pTs[instT] - pTs[instT-1]
+    return 60.0/timeDiff
 
 
 if __name__ == '__main__':
     # mV, time = readFile(test1.csv)
-    mV = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]  # example data
+    mV = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
     time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    PeakVs, PeakTs = getPeaks(mV, time, 2)
+    PeakTs = getPeaks(mV, time, 2)
     print(PeakVs)
     print(PeakTs)
-    print(instHR(PeakVs,PeakTs,1))
+    print(instHR(PeakTs,1))
--- test_instHR.py
+++ test_instHR.py
@@ -7,13 +7,13 @@ def test_getPeaks():
     mV = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
     time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     threshV = 2
-    assert getPeaks(mV, time, threshV) == [3, 3, 3, 3, 7, 11]
+    assert getPeaks(mV, time, threshV) == [3, 7, 11]
     mV2 = [0, 1, 2, 4, 0, 1, 2.5, 3.1, 0, 1, 2, 3]
     time2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    assert getPeaks(mV2, time2, threshV) == [4, 2.5, 3, 3, 6, 11]
+    assert getPeaks(mV2, time2, threshV) == [3, 6, 11]
     mV3 = [0, 1, 2, 4, 0, 1, 2.5, 3.1, 0, 1, 2, 3]
     time3 = [0, 2, 4, 6, 8, 9, 10, 11, 12, 13]
-    assert getPeaks(mV2, time2, threshV) == [4, 2.5, 3, 6, 10, 13]
+    assert getPeaks(mV2, time2, threshV) == [6, 10, 13]
 
 
 def test_instHR():
