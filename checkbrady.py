b8eff509e13772a581e535dd8bcb89a982a34213
Blake Zhai <qz58@duke.edu>
2017-09-23 23:59:41 -0400

    fix some threshold issues

--- checkbrady.py
+++ checkbrady.py
@@ -1,15 +1,14 @@
-def checkbrady(heartrate):
+def checkbrady(heartrate, threshold1=100, threshold2=60):
     """This function checks whether the heart is beating so fast or too slow, 
        if the heart rate is above 100, tachycardia is detected. If the heart
        rate is below 60, bradycardia is detected. """
 
-    if (heartrate > 100):
+    if heartrate > threshol1d1:
         print('There is tachycardia detected')
         return 1
-
-    elif (heartrate < 60):
+    elif heartrate < threshold2:
         print('There is bradycardia detected')
         return 2
-
     else:
         return 3
+
