056cf3b8b58cd3d22854a5aadd6dfff392bd2175
Blake Zhai <qz58@duke.edu>
2017-09-23 22:46:35 -0400

    having some updates

--- checkbrady.py
+++ checkbrady.py
@@ -1,12 +1,14 @@
 def checkbrady(heartrate):
-
+    """This function checks whether the heart is beating so fast or too slow, 
+       if the heart rate is above 100, tachycardia is detected. If the heart
+       rate is below 60, bradycardia is detected."""
 
     if (heartrate > 100):
-        print('There is tachucardia detected')
+        print('There is tachycardia detected')
         return 1
 
     elif (heartrate < 60):
-        print('There is brady detected')
+        print('There is bradycardia detected')
         return 2
 
     else:
