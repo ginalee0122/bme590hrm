b2688bf50e1aea5fb4c1bf20c45814c1b0968e50
jasontrue24 <31495842+jasontrue24@users.noreply.github.com>
2017-09-24 00:06:06 -0400

    Update test_Brady.py

--- test_Brady.py
+++ test_Brady.py
@@ -5,9 +5,9 @@ def test_Brady():
 
     ''' Test CheckBrady with different HR conditions
     1 is Tachy 2 is Brady 3 is healthy'''
-    assert checkbrady(160) == 1
-    assert checkbrady(180) == 1
-    assert checkbrady(40) == 2
-    assert checkbrady(20) == 2
-    assert checkbrady(80) == 3
-    assert checkbrady(90) == 3
+    assert checkbrady(160,100,60) == 1
+    assert checkbrady(180,100,60) == 1
+    assert checkbrady(40,100,60) == 2
+    assert checkbrady(20,100,60) == 2
+    assert checkbrady(80,100,60) == 3
+    assert checkbrady(90,100,60) == 3
